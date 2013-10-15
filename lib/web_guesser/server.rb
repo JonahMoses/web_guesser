1c3cc2fa1ec927297e122813efbab7aba2e5ee65
Jonah <Jonahkmoses@gmail.com>
2013-10-15 13:36:22 -0600

    Finished background color changing

--- lib/web_guesser/server.rb
+++ lib/web_guesser/server.rb
@@ -6,9 +6,10 @@ set :secret, rand(100)
 set :guesses, 5
 
 get '/' do
-  settings.guesses = settings.guesses - 1
+  decrement_guesses
   guess = params[:guess].to_i
   message = feedback_for(settings.secret, guess)
+  output_style = style_for(settings.secret, guess)
 
   if correct_guess(settings.secret, guess) || settings.guesses == 0
     reset_game
@@ -16,9 +17,14 @@ get '/' do
   end
 
   erb  :index, :locals => { :secret_number => settings.secret,
-                            :guess => guess,
-                            :guesses_left => settings.guesses,
-                            :message => message }
+                            :guess         => guess,
+                            :guesses_left  => settings.guesses,
+                            :message       => message,
+                            :output_style  => output_style}
+end
+
+def decrement_guesses
+  settings.guesses = settings.guesses - 1
 end
 
 def correct_guess(number, guess)
@@ -30,17 +36,36 @@ def reset_game
   settings.secret = rand(100)
 end
 
-def feedback_for(number, guess)
+def evaluate_guess(number, guess)
   if guess == number
-    message = "Correct!"
+    :correct
   elsif guess > number + 5
-    "way too high!"
+    :way_too_high
   elsif guess > number
-    "too high!"
+    :too_high
   elsif guess < number - 5
-    "way too low!"
-  elsif guess < number
-    "too low!"
+    :way_too_low
+  else
+    :too_low
   end
 end
 
+def feedback_for(number, guess)
+  case evaluate_guess(number, guess)
+    when :too_high then "too high"
+    when :way_too_high then "way too high"
+    when :too_low then "too low"
+    when :way_too_low then "way too low"
+    else "correct!"
+  end
+end
+
+def style_for(number, guess)
+  case evaluate_guess(number, guess)
+    when :too_high then "too_high"
+    when :way_too_high then "way_too_high"
+    when :too_low then "too_low"
+    when :way_too_low then "way_too_high"
+    when :correct then "correct"
+  end
+end
