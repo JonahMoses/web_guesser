1e2f441a58a99a42477d7765aaf53b87c886f83e
Jonah <Jonahkmoses@gmail.com>
2013-10-15 10:24:38 -0600

    Guessing completed

--- lib/web_guesser/server.rb
+++ lib/web_guesser/server.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+number = rand(100)
+
 get '/' do
-  "Hello, World. The random number is #{ rand(100) }"
+  guess = params[:guess].to_i
+  message = feedback_for(number, guess)
+  erb  :index, :locals => { :secret_number => number,
+                            :guess => guess,
+                            :message => message }
+end
+
+def feedback_for(number, guess)
+  if guess == number
+    message = "Correct!"
+  elsif guess > number + 5
+    "way too high!"
+  elsif guess > number
+    "too high!"
+  elsif guess < number - 5
+    "way too low!"
+  elsif guess < number
+    "too low!"
+  end
+
 end
