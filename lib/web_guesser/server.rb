ae7e30ba0e1d68b521edf72e80bd12d33b09fddd
Jonah <Jonahkmoses@gmail.com>
2013-10-15 11:04:24 -0600

    Added Guess Limiting and Cheat Mode

--- lib/web_guesser/server.rb
+++ lib/web_guesser/server.rb
@@ -1,14 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+set :secret, rand(100)
+
+set :guesses, 5
 
 get '/' do
+  settings.guesses = settings.guesses - 1
   guess = params[:guess].to_i
-  message = feedback_for(number, guess)
-  erb  :index, :locals => { :secret_number => number,
+  message = feedback_for(settings.secret, guess)
+
+  if correct_guess(settings.secret, guess) || settings.guesses == 0
+    reset_game
+    message += " A new number has been generated."
+  end
+
+  erb  :index, :locals => { :secret_number => settings.secret,
                             :guess => guess,
+                            :guesses_left => settings.guesses,
                             :message => message }
+end
+
+def correct_guess(number, guess)
+  guess == number
+end
+
+def reset_game
+  settings.guesses = 5
+  settings.secret = rand(100)
 end
 
 def feedback_for(number, guess)
@@ -23,5 +42,5 @@ def feedback_for(number, guess)
   elsif guess < number
     "too low!"
   end
-
 end
+
